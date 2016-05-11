c252450b2b72bd715dc2890d643cd76105d61059
Lucas Serrano <lucasgserrano@hotmail.com>
2016-05-11 15:47:34 -0300

    Config botao

--- 13/LCD/LCD/src/ili93xx_example.c
+++ 13/LCD/LCD/src/ili93xx_example.c
@@ -95,6 +95,175 @@ static void configure_console(void)
 	stdio_serial_init(CONF_UART, &uart_serial_options);
 }
 
+#define PIN_PUSHBUTTON_1_MASK	PIO_PB3
+#define PIN_PUSHBUTTON_1_PIO	PIOB
+#define PIN_PUSHBUTTON_1_ID		ID_PIOB
+#define PIN_PUSHBUTTON_1_TYPE	PIO_INPUT
+#define PIN_PUSHBUTTON_1_ATTR	PIO_PULLUP | PIO_DEBOUNCE | PIO_IT_RISE_EDGE
+
+#define PIN_PUSHBUTTON_2_MASK	PIO_PC12
+#define PIN_PUSHBUTTON_2_PIO	PIOC
+#define PIN_PUSHBUTTON_2_ID		ID_PIOC
+#define PIN_PUSHBUTTON_2_TYPE	PIO_INPUT
+#define PIN_PUSHBUTTON_2_ATTR	PIO_PULLUP | PIO_DEBOUNCE | PIO_IT_FALL_EDGE
+
+/** IRQ priority for PIO (The lower the value, the greater the priority) */
+#define IRQ_PRIOR_PIO    0
+
+
+/**
+ *  Handle Interrupcao botao 1
+ */
+static void Button1_Handler(uint32_t id, uint32_t mask)
+{
+	
+}
+
+/**
+ *  Handle Interrupcao botao 2.
+ */
+static void Button2_Handler(uint32_t id, uint32_t mask)
+{
+	
+}
+
+/**
+ *  Interrupt handler for TC0 interrupt. 
+ */
+void TC0_Handler(void)
+{
+	volatile uint32_t ul_dummy;
+
+	/* Clear status bit to acknowledge interrupt */
+	//ul_dummy = tc_get_status(TC0,0);
+
+	/* Avoid compiler warning */
+	UNUSED(ul_dummy);
+
+	
+}
+
+/**
+ *  \brief Configure the Pushbuttons
+ *
+ *  Configure the PIO as inputs and generate corresponding interrupt when
+ *  pressed or released.
+ */
+static void configure_buttons(void)
+{
+	pmc_enable_periph_clk(PIN_PUSHBUTTON_1_ID);
+	pmc_enable_periph_clk(PIN_PUSHBUTTON_2_ID);
+	
+	pio_set_input(PIN_PUSHBUTTON_1_PIO, PIN_PUSHBUTTON_1_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_input(PIN_PUSHBUTTON_2_PIO, PIN_PUSHBUTTON_2_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+
+	pio_set_debounce_filter(PIN_PUSHBUTTON_1_PIO, PIN_PUSHBUTTON_1_MASK, 10);
+	pio_set_debounce_filter(PIN_PUSHBUTTON_2_PIO, PIN_PUSHBUTTON_2_MASK, 10);
+	
+	pio_handler_set(PIN_PUSHBUTTON_1_PIO, PIN_PUSHBUTTON_1_ID, PIN_PUSHBUTTON_1_MASK, PIO_IT_FALL_EDGE | PIO_PULLUP, Button1_Handler);
+	pio_handler_set(PIN_PUSHBUTTON_2_PIO, PIN_PUSHBUTTON_2_ID, PIN_PUSHBUTTON_2_MASK, PIO_IT_FALL_EDGE | PIO_PULLUP, Button2_Handler);
+	
+	pio_enable_interrupt(PIN_PUSHBUTTON_1_PIO, PIN_PUSHBUTTON_1_MASK);
+	pio_enable_interrupt(PIN_PUSHBUTTON_2_PIO, PIN_PUSHBUTTON_2_MASK);
+
+	NVIC_SetPriority((IRQn_Type) PIN_PUSHBUTTON_1_ID, 0);
+	NVIC_SetPriority((IRQn_Type) PIN_PUSHBUTTON_2_ID, 0);
+	
+	NVIC_EnableIRQ((IRQn_Type) PIN_PUSHBUTTON_1_ID);
+	NVIC_EnableIRQ((IRQn_Type) PIN_PUSHBUTTON_2_ID);
+}
+
+/**
+ *  Configure Timer Counter 0 to generate an interrupt every 250ms.
+ */
+// [main_tc_configure]
+static void configure_tc(void)
+{
+	/*
+	* Aqui atualizamos o clock da cpu que foi configurado em sysclk init
+	*
+	* O valor atual est'a em : 120_000_000 Hz (120Mhz)
+	*/
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+	
+	/*
+	*	Ativa o clock do periférico TC 0
+	* 
+	*/
+	pmc_enable_periph_clk(ID_TC0);
+
+	/*
+	* Configura TC para operar no modo de comparação e trigger RC
+	* devemos nos preocupar com o clock em que o TC irá operar !
+	*
+	* Cada TC possui 3 canais, escolher um para utilizar.
+	*
+	* Configurações de modo de operação :
+	*	#define TC_CMR_ABETRG (0x1u << 10) : TIOA or TIOB External Trigger Selection 
+	*	#define TC_CMR_CPCTRG (0x1u << 14) : RC Compare Trigger Enable 
+	*	#define TC_CMR_WAVE   (0x1u << 15) : Waveform Mode 
+	*
+	* Configurações de clock :
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK1 : Clock selected: internal MCK/2 clock signal 
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK2 : Clock selected: internal MCK/8 clock signal 
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK3 : Clock selected: internal MCK/32 clock signal 
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK4 : Clock selected: internal MCK/128 clock signal
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK5 : Clock selected: internal SLCK clock signal 
+	*
+	*	MCK		= 120_000_000
+	*	SLCK	= 32_768		(rtc)
+	*
+	* Uma opção para achar o valor do divisor é utilizar a funcao
+	* tc_find_mck_divisor()
+	*/
+	tc_init(TC0,0,TC_CMR_CPCTRG | TC_CMR_TCCLKS_TIMER_CLOCK5);
+	
+	/*
+	* Aqui devemos configurar o valor do RC que vai trigar o reinicio da contagem
+	* devemos levar em conta a frequência que queremos que o TC gere as interrupções
+	* e tambem a frequencia com que o TC está operando.
+	*
+	* Devemos configurar o RC para o mesmo canal escolhido anteriormente.
+	*	
+	*   ^ 
+	*	|	Contador (incrementado na frequencia escolhida do clock)
+	*   |
+	*	|	 	Interrupcao	
+	*	|------#----------- RC
+	*	|	  /
+	*	|   /
+	*	| /
+	*	|-----------------> t
+	*
+	*
+	*/
+	//tc_write_rc(TC0,0,8192);
+	
+	/*
+	* Devemos configurar o NVIC para receber interrupções do TC 
+	*/
+	NVIC_EnableIRQ((IRQn_Type) ID_TC0);
+	
+	/*
+	* Opções possíveis geradoras de interrupção :
+	* 
+	* Essas configurações estão definidas no head : tc.h 
+	*
+	*	#define TC_IER_COVFS (0x1u << 0)	Counter Overflow 
+	*	#define TC_IER_LOVRS (0x1u << 1)	Load Overrun 
+	*	#define TC_IER_CPAS  (0x1u << 2)	RA Compare 
+	*	#define TC_IER_CPBS  (0x1u << 3)	RB Compare 
+	*	#define TC_IER_CPCS  (0x1u << 4)	RC Compare 
+	*	#define TC_IER_LDRAS (0x1u << 5)	RA Loading 
+	*	#define TC_IER_LDRBS (0x1u << 6)	RB Loading 
+	*	#define TC_IER_ETRGS (0x1u << 7)	External Trigger 
+	*/
+	//tc_enable_interrupt(TC0,0,TC_IER_CPCS);
+	
+	//tc_start(TC0, 0);
+}
+
+
 /**
  * \brief Application entry point for smc_lcd example.
  *
@@ -154,8 +323,19 @@ int main(void)
 
 	/** Draw text, image and basic shapes on the LCD */
 	ili93xx_set_foreground_color(COLOR_BLACK);
-	ili93xx_draw_string(10, 20, (uint8_t *)"ili93xx_lcd example");
+	ili93xx_draw_string(10, 20, (uint8_t *)"Nome: Lucas");
+	ili93xx_draw_string(10, 40, (uint8_t *)"Nome: Erica");
+	ili93xx_draw_string(10, 60, (uint8_t *)"Nome: Flavia");
+	
+	ili93xx_set_foreground_color(COLOR_RED);
+	ili93xx_draw_line(0, 90, 240, 90);
+	ili93xx_draw_line(0, 95, 240, 95);
+	
+	ili93xx_set_foreground_color(COLOR_BLACK);
+	ili93xx_draw_string(10, 110, (uint8_t *)"Counter:      ____");
+	ili93xx_draw_string(100, 300, (uint8_t *)"TEMPO: ____");
 
+/*
 	ili93xx_set_foreground_color(COLOR_RED);
 	ili93xx_draw_circle(60, 160, 40);
 	ili93xx_set_foreground_color(COLOR_GREEN);
@@ -165,7 +345,7 @@ int main(void)
 
 	ili93xx_set_foreground_color(COLOR_VIOLET);
 	ili93xx_draw_line(0, 0, 240, 320);
-
+*/
 	while (1) {
 	}
 }
